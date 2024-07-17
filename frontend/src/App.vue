fc09ed580cb694845d0d0958d75ccf4e0b85abbe
Fractiunate// David Rahäuser <78024279+fractiunate@users.noreply.github.com>
2024-07-17 13:49:51 +0200

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { RouterView } from 'vue-router'
+const timelineItems = ref({})
+
+onMounted(async () => {
+  try {
+    timelineItems.value = await (await fetch('/api/timeline')).json()
+  } catch (error) {
+    console.error(error)
+  }
+})
+</script>
+<template>
+  <RouterView />
+</template>
